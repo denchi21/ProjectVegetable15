0a3ff219c4fd3caf254fb86ab0e9e84e1d2ec42f
Alena Havalehko <126282451+AlenaHavaleshko@users.noreply.github.com>
2025-02-24 15:50:50 +0200

    last changes

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -1,9 +1,9 @@
 (() => {
  const refs = {
    // Додати атрибут data-modal-open на кнопку відкриття
-   openMenuBtn: document.querySelector("[data-menu-open]"),
+   openMenuBtn: document.querySelector("[data-mobile-open]"),
    // Додати атрибут data-modal-close на кнопку закриття
-   closeMenuBtn: document.querySelector("[data-menu-close]"),
+   closeMenuBtn: document.querySelector("[data-mobile-close]"),
    // Додати атрибут data-modal на бекдроп моб меню
    menu: document.querySelector("[data-menu]"),
     // Додати атрибут data-overlayl на overlay моб меню
